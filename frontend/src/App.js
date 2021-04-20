b1e64ae7db69c04aaa840c6a7c2e620510dec48d
KoHyunsu <sce06147@naver.com>
2021-04-20 17:47:43 +0900

    [Feat] 에러 페이지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,15 +14,15 @@ function App () {
 
   return (
     <div className="App">
-        <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">{message}</h1>
       </header>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
-    </div>
-  )
+  </div>
+)
 }
 
 export default App;
